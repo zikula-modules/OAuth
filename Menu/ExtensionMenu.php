4a16ce7465e0b379a0b1ffa98d679966bbdb15b4
Craig Heydenburg <craigh@mac.com>
2020-01-19 19:54:32 -0500

    add extensionMenu

--- Menu/ExtensionMenu.php
+++ Menu/ExtensionMenu.php
@@ -11,29 +11,24 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Zikula\OAuthModule\Container;
+namespace Zikula\OAuthModule\Menu;
 
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Contracts\Translation\TranslatorInterface;
-use Zikula\Core\LinkContainer\LinkContainerInterface;
+use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Zikula\MenuModule\ExtensionMenu\ExtensionMenuInterface;
 use Zikula\OAuthModule\OAuthConstant;
-use Zikula\PermissionsModule\Api\PermissionApi;
+use Zikula\PermissionsModule\Api\ApiInterface\PermissionApiInterface;
 use Zikula\UsersModule\Collector\AuthenticationMethodCollector;
 
-class LinkContainer implements LinkContainerInterface
+class ExtensionMenu implements ExtensionMenuInterface
 {
     /**
-     * @var TranslatorInterface
+     * @var FactoryInterface
      */
-    private $translator;
+    private $factory;
 
     /**
-     * @var RouterInterface
-     */
-    private $router;
-
-    /**
-     * @var PermissionApi
+     * @var PermissionApiInterface
      */
     private $permissionApi;
 
@@ -43,54 +38,48 @@ class LinkContainer implements LinkContainerInterface
     private $collector;
 
     public function __construct(
-        TranslatorInterface $translator,
-        RouterInterface $router,
-        PermissionApi $permissionApi,
+        FactoryInterface $factory,
+        PermissionApiInterface $permissionApi,
         AuthenticationMethodCollector $collector
     ) {
-        $this->translator = $translator;
-        $this->router = $router;
+        $this->factory = $factory;
         $this->permissionApi = $permissionApi;
         $this->collector = $collector;
     }
 
-    public function getLinks(string $type = LinkContainerInterface::TYPE_ADMIN): array
+    public function get(string $type = self::TYPE_ADMIN): ?ItemInterface
     {
         $method = 'get' . ucfirst(mb_strtolower($type));
         if (method_exists($this, $method)) {
             return $this->{$method}();
         }
 
-        return [];
+        return null;
     }
 
-    /**
-     * Get the admin links for this extension.
-     */
-    private function getAdmin(): array
+    private function getAdmin(): ?ItemInterface
     {
-        $links = [];
+        $menu = $this->factory->createItem('oauthAdminMenu');
         if ($this->permissionApi->hasPermission('ZikulaOAuthModule::', '::', ACCESS_ADMIN)) {
-            $links[] = [
-                'url' => $this->router->generate('zikulaoauthmodule_mapping_list'),
-                'text' => $this->translator->trans('Mapping list'),
-                'icon' => 'list'
-            ];
+            $menu->addChild('Mapping list', [
+                'route' => 'zikulaoauthmodule_mapping_list',
+            ])->setAttribute('icon', 'fas fa-list');
+
             $methods = [OAuthConstant::ALIAS_GITHUB, OAuthConstant::ALIAS_GOOGLE, OAuthConstant::ALIAS_FACEBOOK, OAuthConstant::ALIAS_LINKEDIN];
             foreach ($methods as $method) {
                 $authMethod = $this->collector->get($method);
                 if (null === $authMethod) {
                     continue;
                 }
-                $links[] = [
-                    'url' => $this->router->generate('zikulaoauthmodule_config_settings', ['method' => $method]),
-                    'text' => $authMethod->getDisplayName() . ' ' . $this->translator->trans('settings'),
-                    'icon' => 'wrench'
-                ];
+                $menu->addChild('%auth_method% settings', [
+                    'route' => 'zikulaoauthmodule_config_settings',
+                    'routeParameters' => ['method' => $method]
+                ])->setExtra('translation_params', ['%auth_method%' => $authMethod->getDisplayName()])
+                    ->setAttribute('icon', 'fas fa-wrench');
             }
         }
 
-        return $links;
+        return 0 === $menu->count() ? null : $menu;
     }
 
     public function getBundleName(): string
